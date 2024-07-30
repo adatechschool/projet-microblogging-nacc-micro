a9b190a8f9cc2b90fe5ec767e2fb881bdf4b1171
CarlosPTN <carlosmioli1@gmail.com>
2024-07-30 10:27:22 +0000

    changes to store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -27,6 +28,24 @@ class PostController extends Controller
         return view('post.edit', [
             'user' => $request->user(),
         ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'image_path' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        Post::create($request->all());
+        return redirect()->route('posts.index')
+                         ->with('success', 'Post created successfully.');
+    }
+    public function update(Request $request): View{
+
+        $posts=Post::all();
+        return view('posts.index', compact('posts'));
+        
     }
 
 };
