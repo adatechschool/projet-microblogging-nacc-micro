46951bacfe56a38677c5eedb55e7999c8deb420b
Anael Toula <anael.toula@gmail.com>
2024-08-19 14:23:32 +0000

    tests fonctionnels

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,25 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+    public function test_wrong_password(): void
+    {
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('/login', ['email' => 'kill@gmail.com', 'password'=> 'toto']);
+        
+      $response->assertStatus(302);
+    }
+
+    public function test_right_password(): void{
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('/login', ['email' => 'kill@gmail.com', 'password'=> 'azertyui']);
+ 
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('dashboard');
     }
 }
